48b681bbad3fb1196ef2ecb1c7bd350f2f035a13
brisberg <brandon.risberg@gmail.com>
2020-07-06 19:53:07 -0700

    Using bodyparser

--- server/server.ts
+++ server/server.ts
@@ -1,11 +1,14 @@
-import express = require('express');
+import bodyParser from 'body-parser';
+import express from 'express';
 
 // Create a new express app instance
 const app: express.Application = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-app.listen(3000, () => {
+app.listen({port: 3000}, () => {
   console.log('App is listening on port 3000!');
 });
